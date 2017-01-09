70112e69f82e0b04326414af5af6e2f8ed33b306
dedChar <unnamed0736@gmail.com>
2017-01-09 21:00:55 +0100

    Removed Rebirth version and BR patch and moved AB Version and room files to _Old, also added the AB+ code, currently only YellowBlocks are implemented

---
+++ YellowBlock.lua
@@ -0,0 +1,132 @@
+--StartDebug()
+
+--Yellow Block
+yellowblock = {};
+yb_entitytype = Isaac.GetEntityTypeByName("Yellow Block");
+--debug_text = "";
+
+-- Movement
+function yellowblock:ai_main(npc)
+	local room = Game():GetRoom();
+	--local getcol = room.GetGridCollisionAtPos(room, Vector(Isaac.GetPlayer(0).Position.X, Isaac.GetPlayer(0).Position.Y - 15));
+	--debug_text = npc.Velocity.Y;
+	--debug_text = npc.EntityGridCollisionClass;
+	--debug_text = npc.StateFrame;
+	--debug_text = "X: " .. npc.Position.X .. " || Y: " .. npc.Position.Y;
+	--debug_text = npc.Velocity.X .. " | " .. npc.Velocity.Y;
+	
+	if (npc.State == NpcState.STATE_INIT) then
+		npc:AddEntityFlags(EntityFlag.FLAG_NO_FLASH_ON_DAMAGE)
+		npc:AddEntityFlags(EntityFlag.FLAG_NO_DEATH_TRIGGER)
+		npc:AddEntityFlags(EntityFlag.FLAG_NO_TARGET)
+		npc:AddEntityFlags(EntityFlag.FLAG_NO_STATUS_EFFECTS)
+		npc:AddEntityFlags(EntityFlag.FLAG_DONT_OVERWRITE)
+		npc.CanShutDoors = false;
+		npc.CollisionDamage = 0.0;
+    
+		npc:SetSpriteFrame("No-Spikes", 10);
+		if ((npc.Variant == 1 and npc.SubType == 0) or (npc.Variant == 0 and npc.SubType == 0)) then
+			npc.V1 = Vector(3,3);
+		elseif ((npc.Variant == 2 and npc.SubType == 0) or (npc.Variant == 0 and npc.SubType == 1)) then
+			npc.V1 = Vector(-3,3);
+		elseif ((npc.Variant == 3 and npc.SubType == 0) or (npc.Variant == 0 and npc.SubType == 2)) then
+			npc.V1 = Vector(3,-3);
+		elseif ((npc.Variant == 4 and npc.SubType == 0) or (npc.Variant == 0 and npc.SubType == 3)) then
+			npc.V1 = Vector(-3,-3);
+		else
+			npc.V1 = Vector(3,3);
+		end
+		npc.State = NpcState.STATE_MOVE;
+	end
+	
+	if (npc.State == NpcState.STATE_MOVE) then
+		npc.Velocity = npc.V1;
+	end
+	
+	if (npc.StateFrame == 30 and npc.State ~= NpcState.STATE_IDLE) then
+		npc:PlaySound(237, 1.0, 0, false, 1.0)
+		npc.SetSpriteFrame(npc ,"Spikes", 0);
+		npc.CollisionDamage = 1.0;
+	end
+	
+	if (room:IsClear() == true and npc.Variant == 0) then
+		npc.State = NpcState.STATE_IDLE;
+	end
+	
+	if (npc.State == NpcState.STATE_IDLE) then
+		npc:SetSpriteFrame("No-Spikes", 10);
+		--npc.Velocity = Vector(0,0);
+		npc.CollisionDamage = 0.0;
+		npc.Friction = 0.5;
+		npc.CollisionDamage = 0.0;
+	end
+	
+	
+	npc.StateFrame = npc.StateFrame + 1;
+	
+end
+
+--Wall Bounce
+function yellowblock:ai_turn(entity);
+	local room = Game():GetRoom();
+  
+	local col_up = room.GetGridCollisionAtPos(room, Vector(entity.Position.X, entity.Position.Y - 20));
+	local col_down = room.GetGridCollisionAtPos(room, Vector(entity.Position.X, entity.Position.Y + 20));
+	local col_left = room.GetGridCollisionAtPos(room, Vector(entity.Position.X - 20, entity.Position.Y));
+	local col_right = room.GetGridCollisionAtPos(room, Vector(entity.Position.X + 20, entity.Position.Y));
+	
+	--debug_text = entity.V1.X .. " | " .. entity.V1.Y .. " isfirstvisit(): " .. tostring(room:IsFirstVisit());
+	
+		if(col_up == 1 or 
+		   col_up == 3 or 
+		   col_up == 4) then
+		   
+		   --debug_text = debug_text .. "u";
+
+		  entity.V1 = Vector(entity.V1.X, 3);
+		end  
+		
+		if(col_down == 1 or 
+		   col_down == 3 or 
+		   col_down == 4) then
+		   
+		   --debug_text = debug_text .. "d";
+
+		   entity.V1 = Vector(entity.V1.X, -3);
+		end
+
+		if(col_left == 1 or
+		   col_left == 3 or 
+		   col_left == 4) then
+		   
+		   --debug_text = debug_text .. "l";
+
+		   entity.V1 = Vector(3, entity.V1.Y);
+		end  
+		
+		if(col_right == 1 or
+		   col_right == 3 or 
+		   col_right == 4) then
+		   
+		   --debug_text = debug_text .. "r";
+
+		   entity.V1 = Vector(-3, entity.V1.Y);
+		end  	
+end
+
+
+-- Debug Render
+--function yellowblock:dbgtext()
+--
+--	for i = 1, #spwn_list do
+--		Isaac.RenderText(spwn_list[i], 20, 80 + (i * 10), 255, 255, 255, 255);
+--	end
+--
+--	Isaac.RenderText(yb_entitytype, 200, 0, 255, 255, 255, 255);
+--	Isaac.RenderText(debug_text, 40, 50, 255, 255, 0, 255);
+--end	
+
+-- Register the callbacks
+Agony:AddCallback(ModCallbacks.MC_NPC_UPDATE, yellowblock.ai_main, yb_entitytype);
+Agony:AddCallback(ModCallbacks.MC_NPC_UPDATE, yellowblock.ai_turn, yb_entitytype);
+--yellowblock:AddCallback(ModCallbacks.MC_POST_RENDER, yellowblock.dbgtext);
---
+++ main.lua
@@ -0,0 +1,101 @@
+Agony = RegisterMod("The Agony of Isaac", 1);
+StartDebug();
+--https://www.reddit.com/r/themoddingofisaac/comments/5ml25i/how_to_make_it_so_a_stat_modification_only_lasts/dc6uhnf/
+--needed to load other lua files right now and it only works when --luadebug is set in game properties
+--later nicalis will add require() preoperly
+function Include(aFilename)
+  local sourcePath = debug.getinfo(1, "S").source:sub(2)
+  local baseDir = sourcePath:match(".*/") or "./"
+
+  dofile( ("%s%s"):format(baseDir, aFilename) )
+end
+
+Include("YellowBlock.lua");
+
+--respawnV2's vars
+local ent_before = {};
+local rspwn_allow = false;
+local spwn_list = {};
+local level_seed_backup = 666;
+
+function Agony:respawnV2()
+	local room = Game():GetRoom();
+	local level = Game():GetLevel();
+
+	local level_seed = level:GetDungeonPlacementSeed(); --level identification
+	local room_seed = room:GetSpawnSeed(); --for room identification
+	
+	--debug_text = tostring(room:IsClear()) .. " " .. tostring(room:IsFirstVisit()) --room_seed .. " | " .. level_seed .. " | GameFrmcnt: " .. Game():GetFrameCount();-- " | RoomFrmcnt: " .. room:GetFrameCount() .. " | GameFrmcnt: " .. Game():GetFrameCount() .. " | IsClear: " .. tostring(room:IsClear());
+	--debug_text = Isaac.LoadModData(Agony) .. " | " .. tostring(Isaac.HasModData(Agony));
+	
+	if(Isaac.HasModData(Agony) == true and Game():GetFrameCount() > 1 and level_seed_backup == 666) then --restore data when continuing run after having the game closed
+		spwn_list = string_totable(Isaac.LoadModData(Agony));
+		level_seed_backup = level_seed;
+		rspwn_allow = true;
+	end
+	
+	if(Game():GetFrameCount() == 1 or (level_seed ~= level_seed_backup and Game():GetFrameCount() > 1)) then --reset vars and delete savedata when restarting run and entering a new level
+		spwn_list = {};
+		rspwn_allow = false;
+		ent_before = {};
+		Isaac.RemoveModData(Agony);
+		level_seed_backup = level_seed;
+	end
+
+	for i = 1, #spwn_list, 6 do
+		--Isaac.RenderText(spwn_list[i], 400, 100 + ((i / 6) *10), 255, 0, 0, 255);
+		if (room:IsFirstVisit() == false and room:IsClear() == true and spwn_list[i] == room_seed and room:GetFrameCount() == 1) then
+			rspwn_allow = true;
+		end	
+	end
+	
+	if (room:IsFirstVisit() == false and room:IsClear() == true and rspwn_allow == true) then --run only if this room truly has been visited before and actually cleared (not skipped with e.g. bombs)
+		for i = 1, #spwn_list, 6 do
+			if (spwn_list[i] == room_seed) then
+				Isaac.Spawn(spwn_list[i+1], spwn_list[i+2], spwn_list[i+3], Vector(spwn_list[i+4], spwn_list[i+5]), Vector(0,0), Isaac.GetPlayer(0));
+			end	
+		end
+		rspwn_allow = false; --avoid looping the respawn and remove
+	elseif (room:IsFirstVisit() == true and room:GetFrameCount() == 1) then --check if new room has respawnable entity inside, only once on the first room entry
+		ent_before = Isaac.GetRoomEntities();
+		for i = 1, #ent_before do
+			if (ent_before[i].Type == yb_entitytype) then
+				table.insert(spwn_list, 1, room_seed);
+				table.insert(spwn_list, 2, ent_before[i].Type);
+				table.insert(spwn_list, 3, ent_before[i].Variant);
+				table.insert(spwn_list, 4, ent_before[i].SubType);
+				table.insert(spwn_list, 5, ent_before[i].Position.X);
+				table.insert(spwn_list, 6, ent_before[i].Position.Y);
+			end
+		end
+		Isaac.SaveModData(Agony, table_tostring(spwn_list)); --save the latest table for the case of game exit
+	end
+end
+
+function table_tostring(t)
+	local s = "";
+	for i = 1, #t do
+		s = s .. t[i] .. " ";
+	end
+	s = s:sub(1, -2) --remove last space
+	
+	return s;
+end
+
+function string_totable(s)
+	local t = {};
+	for part in string.gmatch(s, "%S+") do
+		t[#t + 1] = tonumber(part);
+	end
+	
+	return t;
+end
+
+--Agony END
+
+
+
+
+--Callbacks
+Agony:AddCallback(ModCallbacks.MC_POST_UPDATE, Agony.respawnV2);
+
