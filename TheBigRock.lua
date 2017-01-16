8fb78e7199eedf458ce61eafcd1a8681774cf378
ProbablyTez <GiulianoOliveri@outlook.com>
2017-01-16 12:05:12 +0100

    Added Item The Big Rock

---
+++ TheBigRock.lua
@@ -0,0 +1,17 @@
+local item_TheBigRock = Isaac.GetItemIdByName("The Big Rock")
+local theBigRock =  {}
+
+--Grants + 2.69 DMG and -0.4 SPD
+function theBigRock:cacheUpdate (player,cacheFlag)
+	local player = Isaac.GetPlayer(0);
+	if (player:HasCollectible(item_TheBigRock)) then
+		if (cacheFlag == CacheFlag.CACHE_DAMAGE) then
+		player.Damage = player.Damage + 3.69;
+		end
+		if (cacheFlag == CacheFlag.CACHE_SPEED) then
+		player.MoveSpeed = player.MoveSpeed - 0.5;
+		end
+	end
+end
+
+Agony:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, theBigRock.cacheUpdate);
