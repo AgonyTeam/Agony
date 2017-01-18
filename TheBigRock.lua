6d90909aa1433c5993f82a95f1d65b4cd2412208
ProbablyTez <GiulianoOliveri@outlook.com>
2017-01-18 22:46:11 +0100

    Fixed Item The Big Rock
    The Big Rock now has 1 in 20 chances to spawn instead of a small rock

--- TheBigRock.lua
+++ TheBigRock.lua
@@ -1,5 +1,8 @@
 local item_TheBigRock = Isaac.GetItemIdByName("The Big Rock")
-local theBigRock =  {}
+local theBigRock =  {
+	hasTriedToMorphSmallRock = false,
+	seed = nil
+}
 
 --Grants + 2.69 DMG and -0.4 SPD
 function theBigRock:cacheUpdate (player,cacheFlag)
@@ -13,4 +16,34 @@ function theBigRock:cacheUpdate (player,cacheFlag)
 	end
 end
 
-Agony:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, theBigRock.cacheUpdate);
+--[[function theBigRock:debug()
+	local entList = Isaac.GetRoomEntities()
+	for i = 1, #entList, 1 do
+		Isaac.RenderText(tostring(entList[i].Type) .. " " .. tostring(entList[i].Variant) .. " " .. tostring(entList[i].SubType), 100, 10 + (10*i), 255, 0, 0, 255)
+	end
+	Isaac.RenderText(tostring(item_TheBigRock), 400, 100, 255, 255, 255, 255)
+end--]]
+
+
+-- Morphs the small rock item 1/20th of the time into the big rock
+function theBigRock:morphSmallRock()
+	if theBigRock.seed ~= RNG():GetSeed() then
+		theBigRock.hasTriedToMorphSmallRock = false
+		theBigRock.seed = RNG():GetSeed()
+	end
+	if theBigRock.hasTriedToMorphSmallRock == false then
+		local entList = Isaac.GetRoomEntities()
+		for i = 1, #entList, 1 do
+			if entList[i].Type == 5 and entList[i].Variant == 100 and entList[i].SubType == 90 then
+				if RNG():GetSeed()%20 == 1 then
+					entList[i]:ToPickup():Morph(entList[i].Type, entList[i].Variant, item_TheBigRock, true)
+				end
+				theBigRock.hasTriedToMorphSmallRock = true
+			end
+		end
+	end
+end
+
+Agony:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, theBigRock.cacheUpdate)
+--Agony:AddCallback(ModCallbacks.MC_POST_RENDER, theBigRock.debug)
+Agony:AddCallback(ModCallbacks.MC_POST_UPDATE, theBigRock.morphSmallRock)
