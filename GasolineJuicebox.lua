fe752a048354324b4314a8f0d638668775871645
dedChar <unnamed0736@gmail.com>
2017-01-20 21:26:13 +0100

    fix issue #59 * restricted tear replacement only to tears created by player and incubus * added synergy of incubus + gasoline jb + bffs!

--- GasolineJuicebox.lua
+++ GasolineJuicebox.lua
@@ -29,30 +29,38 @@ function gasolinejb:TearsToFlames()
 	local entities = Isaac.GetRoomEntities();
 	local velocity = nil;
 	local pos = nil;
+	local tearParent = nil;
 	
 	--Replace tears with fires
 	if (player:HasCollectible(item_GasolineJuicebox) == true) then
 		for i = 1, #entities do
-			if (entities[i].Type == EntityType.ENTITY_TEAR) then
+			if (entities[i].Type == EntityType.ENTITY_TEAR and (entities[i].Parent.Type == 1 or (entities[i].Parent.Type == 3 and entities[i].Parent.Variant == 80))) then --player == 1.0.0, incubus == 3.80.0
 				
 				velocity = entities[i].Velocity;
 				pos = entities[i].Position;
+				tearParent = entities[i].Parent;
 				
 				entities[i]:Remove();
 				
-				local fire = Isaac.Spawn(1000, 52, 1, pos, velocity, player);
+				local fire = Isaac.Spawn(1000, 52, 1, pos, velocity, tearParent);
 				fire.CollisionDamage = player.Damage;
 				if (player:HasCollectible(CollectibleType.COLLECTIBLE_CANDLE)) then --blue fires if the player has blue candle
 					fire:GetSprite():ReplaceSpritesheet(0, "gfx/effects/effect_005_fire_blue.png")
 					fire:GetSprite():LoadGraphics();
 				end
 				
+				
+				if (player:HasCollectible(CollectibleType.COLLECTIBLE_BFFS) and fire.SpawnerType == 3 and fire.SpawnerVariant == 80) then --double the damage of incubus' fires if player has bffs!
+					fire.CollisionDamage = player.Damage*2;
+				end
+				
 				pos = nil;
 				velocity = nil;
+				tearParent = nil;
 			end
 			
 			--Fires dissappear after time, unless isaac has the red candle
-			if (entities[i].Type == 1000 and entities[i].Variant == 52 and entities[i].SpawnerType == 1 and entities[i].SubType == 1 and not player:HasCollectible(CollectibleType.COLLECTIBLE_RED_CANDLE)) then
+			if (entities[i].Type == 1000 and entities[i].Variant == 52 and (entities[i].SpawnerType == 1 or entities[i].SpawnerType == 3) and entities[i].SubType == 1 and not player:HasCollectible(CollectibleType.COLLECTIBLE_RED_CANDLE)) then
 				if (entities[i].FrameCount % 100 == 0) then
 					if (entities[i]:GetSprite():IsPlaying("FireStage00") == true) then
 						entities[i]:GetSprite():Play("FireStage01");
@@ -67,7 +75,7 @@ function gasolinejb:TearsToFlames()
 			end
 			
 			--Fires have homing effect when the player has blue candle
-			if (entities[i].Type == 1000 and entities[i].Variant == 52 and entities[i].SpawnerType == 1 and entities[i].SubType == 1 and player:HasCollectible(CollectibleType.COLLECTIBLE_CANDLE)) then
+			if (entities[i].Type == 1000 and entities[i].Variant == 52 and (entities[i].SpawnerType == 1 or entities[i].SpawnerType == 3) and entities[i].SubType == 1 and player:HasCollectible(CollectibleType.COLLECTIBLE_CANDLE)) then
 				entities[i].Velocity = entities[i].Velocity:__add(Agony:calcTearVel(entities[i].Position, (Agony:getNearestEnemy(entities[i])).Position, 0.5))
 			end
 		end
