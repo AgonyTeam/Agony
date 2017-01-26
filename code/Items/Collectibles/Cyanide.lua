80f164d57dacf6daf2c6b336be9d77bd032209cf
ProbablyTez <GiulianoOliveri@outlook.com>
2017-01-26 18:59:32 +0100

    Added Items Suicide God and Cyanide

--- code/Items/Collectibles/Cyanide.lua
+++ code/Items/Collectibles/Cyanide.lua
@@ -4,7 +4,10 @@ local cyanide =  {}
 function cyanide:killPlayer()
 	local player = Game():GetPlayer(0)
 	player:RemoveCollectible(item_Cyanide)
-	player:Kill()
+	for i = 1, 5, 1 do
+		Game():GetLevel():SetStage(LevelStage.STAGE7, StageType.STAGETYPE_AFTERBIRTH)
+		Game():StartStageTransition(true, 1)
+	end
 end
 
 Agony:AddCallback(ModCallbacks.MC_USE_ITEM, cyanide.killPlayer, item_Cyanide)
---
+++ code/Items/Collectibles/SuicideGod.lua
@@ -0,0 +1,34 @@
+local item_SuicideGod = Isaac.GetItemIdByName("Suicide God");
+local suicideGod =  {
+	hasItem = nil, --used for costume
+	costumeID = nil
+}
+suicideGod.costumeID = Isaac.GetCostumeIdByPath("gfx/characters/costume_suicidegod.anm2")
+
+function suicideGod:checkIfPlayerDying()
+	local player = Game():GetPlayer(0)
+	if player:HasCollectible(item_SuicideGod) then
+		local sprite = player:GetSprite()
+
+		if sprite:IsPlaying("Death") and sprite:GetFrame() == 1 then
+			Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, 0, Isaac.GetFreeNearPosition(player.Position, 50), Vector (0,0), player)
+			for j = 1, 5, 1 do
+	   	    	Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_NULL, 0, Isaac.GetFreeNearPosition(player.Position, 50), Vector (0,0), player)
+	   	    end
+		end
+	end
+end
+
+function suicideGod:onUpdate(player)
+	if Game():GetFrameCount() == 1 then
+		suicideGod.hasItem = false
+	end
+	if player:HasCollectible(item_SuicideGod) then
+		if suicideGod.hasItem == false then
+			--player:AddNullCostume(suicideGod.costumeID)
+			suicideGod.hasItem = true
+		end
+	end
+end
+Agony:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, suicideGod.onUpdate)
+Agony:AddCallback(ModCallbacks.MC_POST_UPDATE, suicideGod.checkIfPlayerDying)
