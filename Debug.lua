8c6638fbbdad1ccfb5b42235208b0652fa312269
dedChar <unnamed0736@gmail.com>
2017-02-20 20:41:11 +0100

    added universal debug output for tables

--- Debug.lua
+++ Debug.lua
@@ -1,6 +1,8 @@
 local debugScript = {}
 
 debug_text = "ayy"
+debug_tbl1 = {one_no = "entries"}
+debug_tbl2 = {two_no = "entries"}
 
 function debugScript:displayEntities()
 	local entList = Isaac.GetRoomEntities()
@@ -13,5 +15,19 @@ function debugScript:universalDebugText()
 	Isaac.RenderText(debug_text, 40, 250, 255, 255, 0, 255);
 end
 
+function debugScript:universalTableParser()
+	local count = 0
+	local count2 = 0
+	for a,b in pairs(debug_tbl1) do
+		Isaac.RenderText(tostring(a).. ": " .. tostring(b), 150, 10 + count*10, 0, 255, 0, 255)
+		count = count + 1
+	end
+	for a,b in pairs(debug_tbl2) do
+		Isaac.RenderText(tostring(a).. ": " .. tostring(b), 150, 10 + count2*10 + count*10, 255, 0, 255, 255)
+		count2 = count2 + 1
+	end
+end
+
 Agony:AddCallback(ModCallbacks.MC_POST_RENDER, debugScript.displayEntities)
 Agony:AddCallback(ModCallbacks.MC_POST_RENDER, debugScript.universalDebugText)
+Agony:AddCallback(ModCallbacks.MC_POST_RENDER, debugScript.universalTableParser)
