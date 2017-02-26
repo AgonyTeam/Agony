1cddc70ac2f82d1f08627a8bbc44b3fbb6c590db
dedChar <unnamed0736@gmail.com>
2017-02-26 15:03:59 +0100

    prevent eternal pooters from doing the regular attack * fixes #246

--- code/Monsters/Eternals/Pooter.lua
+++ code/Monsters/Eternals/Pooter.lua
@@ -1,26 +1,27 @@
  EternalPooter = {};
 
---Eternal Round Worms
+--Eternal Pooters
 function EternalPooter:ai_main(entity)
 	local room = Game():GetRoom();
 	local sprite = entity:GetSprite();
-	
+
 	--Replace regular entity with eternal version
 	if (entity.Type == EntityType.ENTITY_POOTER and math.random(10) == 1 and room:GetFrameCount() <= 10 and entity.SubType ~= 15) then
 		entity.SubType = 15;
 		sprite:Load("gfx/Monsters/Eternals/Pooter/animation.anm2", true);
-		entity.HitPoints = 20;
+		entity.HitPoints = 16;
 		
 	end
 	
 	if (entity.Type == EntityType.ENTITY_POOTER and entity.SubType == 15 and entity.State == NpcState.STATE_ATTACK) then
+		entity.ProjectileDelay = -1 --prevent original shot
 		local PlayerPos = entity:GetPlayerTarget().Position;
-
-		if (sprite:IsEventTriggered("EternalShoot") == true) then
-			entity.ProjectileDelay = 999
+		if (sprite:IsEventTriggered("EternalShoot")) then
 			--entity:PlaySound(SoundEffect.SOUND_NULL, 1.0, 0, false, 1.0)
-			Isaac.Spawn(EntityType.ENTITY_PROJECTILE, 6, 0, entity.Position, (Agony:calcTearVel(entity.Position, PlayerPos, 13)):Rotated(math.random(20)-10), entity);
+			local t = Isaac.Spawn(EntityType.ENTITY_PROJECTILE, 6, 0, entity.Position, (Agony:calcTearVel(entity.Position, PlayerPos, 13)):Rotated(math.random(20)-10), entity);
+			t.SpawnerEntity = entity 
 		end
+		
 	end
 end
 
