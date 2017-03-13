4948b4bb2ece400951a0861bea96556b41356928
dedChar <unnamed0736@gmail.com>
2017-03-13 19:01:13 +0100

    tiny tiny horn: change animation handling code to better one

--- code/Familiars/TinyTinyHorn.lua
+++ code/Familiars/TinyTinyHorn.lua
@@ -2,20 +2,34 @@ CollectibleType["AGONY_C_TINY_TINY_HORN"] = Isaac.GetItemIdByName("Tiny Tiny Hor
 FamiliarVariant["AGONY_F_TINY_TINY_HORN"] = Isaac.GetEntityVariantByName("Tiny Tiny Horn")
 
 local TinyTinyHorn = {
-	Cooldown = 60,
-	MaxOrbCount = 3--[[,
+	maxCooldown = 60,
+	MaxOrbCount = 3,
+	maxAnimCooldown = 8--[[,
 	dbg = {no = "entries"},
 	dbg2 = {data = "orbs"}]]--
 }
+--Orbspawner helper function
+local function spawnOrb(fam)
+	local famData = fam:GetData()
+	local orb = Isaac.Spawn(EntityType.ENTITY_LITTLE_HORN, 1, 0, fam.Position, Vector(0,0), nil)
+	orb:ToNPC().Scale = 0.5
+	orb:SetSize(1, Vector(1,1), 8) --need this to set the numGridCollisionPoints
+	orb:AddEntityFlags(EntityFlag.FLAG_NO_TARGET | EntityFlag.FLAG_DONT_OVERWRITE | EntityFlag.FLAG_CHARM | EntityFlag.FLAG_FRIENDLY | EntityFlag.FLAG_NO_STATUS_EFFECTS )
+	orb.MaxHitPoints = 5
+	orb.HitPoints = 5
+	
+	famData.Orbs[#famData.Orbs+1] = orb --save it to orb-table of the familiar
+end
+
 
 --main behaviour function
 function TinyTinyHorn:updateFam(fam)
-	local data = fam:GetData()
+	local famData = fam:GetData()
 	local player = Isaac.GetPlayer(0)
 	local fireDir = player:GetFireDirection()
 	local famSprite = fam:GetSprite()
 	
-	if data.Cooldown <= 0 and #data.Orbs < TinyTinyHorn.MaxOrbCount and fireDir ~= Direction.NO_DIRECTION then --if the cooldown is 0, the orb per familiar cap isn't reached and the player is actually firing
+	if famData.Cooldown <= 0 and fireDir ~= Direction.NO_DIRECTION and #famData.Orbs < TinyTinyHorn.MaxOrbCount then
 		famSprite.FlipX = false
 		if fireDir == Direction.DOWN then
 			famSprite:Play("FloatShootDown")
@@ -27,37 +41,63 @@ function TinyTinyHorn:updateFam(fam)
 		elseif fireDir == Direction.RIGHT then
 			famSprite:Play("FloatShootSide")
 		end
-		
-		--spawn the orb
-		local orb = Isaac.Spawn(EntityType.ENTITY_LITTLE_HORN, 1, 0, fam.Position, Vector(0,0), nil)
-		orb:ToNPC().Scale = 0.5
-		orb:SetSize(1, Vector(1,1), 8) --need this to set the numGridCollisionPoints
-		orb:AddEntityFlags(EntityFlag.FLAG_NO_TARGET | EntityFlag.FLAG_DONT_OVERWRITE | EntityFlag.FLAG_CHARM | EntityFlag.FLAG_NO_STATUS_EFFECTS )
-		orb:ToNPC().CanShutDoors = false
-		orb.CollisionDamage = 0 --this apparently only affects the player, enemies are still damaged through charm
-		
-		local tmpTarget = Agony:getNearestEnemy(orb) --target nearest enemy
-		if tmpTarget == orb or tmpTarget == player then --target the familiar if there are no enemies or the player gets targeted
-			orb.Target = fam
-		else
-			orb.Target = tmpTarget
-		end
-		
-		data.Orbs[#data.Orbs+1] = orb --save it to orb-table of the familiar
-		data.Cooldown = TinyTinyHorn.Cooldown --reset cooldown
-	elseif data.Cooldown > 0 then
-		if famSprite:IsFinished("FloatShootDown") or famSprite:IsFinished("FloatShootUp") or famSprite:IsFinished("FloatShootSide") then
-			fam:GetSprite():Play("FloatDown")
-		end
-	
-		data.Cooldown = data.Cooldown - 1
+		famData.Cooldown = TinyTinyHorn.maxCooldown
+		famData.animCooldown = TinyTinyHorn.maxAnimCooldown
+		spawnOrb(fam)
 	end
 	
-	for entry, orb in pairs(data.Orbs) do
+	if famData.Cooldown > 0 then
+		famData.Cooldown = famData.Cooldown - 1
+	end
+	
+	if famData.animCooldown == 0 then                                                                                         --------------------------------------------
+		if fireDir == Direction.DOWN and not famSprite:IsPlaying("FloatDown") then                                            --  This stops the FloatShoot* anims when	--
+			famSprite:Play("FloatDown")                                                                                       --  animCooldown is 0	  					--
+			famSprite.FlipX = false                                                                                           --------------------------------------------
+		elseif fireDir == Direction.UP and not famSprite:IsPlaying("FloatUp") then                                            
+			famSprite:Play("FloatUp")                                                                                         
+			famSprite.FlipX = false
+		elseif fireDir == Direction.LEFT and not (famSprite:IsPlaying("FloatSide") and famSprite.FlipX) then
+			famSprite:Play("FloatSide")
+			famSprite.FlipX = true --only one animation for the side, have to flip it for left
+		elseif fireDir == Direction.RIGHT and not (famSprite:IsPlaying("FloatSide") and not famSprite.FlipX) then
+			famSprite:Play("FloatSide")
+			famSprite.FlipX = false
+		end
+	else
+		if fireDir == Direction.DOWN and not famSprite:IsPlaying("FloatShootDown") then                                         ------------------------------------------ 
+			famSprite:Play("FloatShootDown")                                                                                    --	This block adjusts the anims while	-- 
+			famSprite.FlipX = false                                                                                             --	the familiar is shooting, but the   -- 
+		elseif fireDir == Direction.UP and not famSprite:IsPlaying("FloatShootUp") then                                         --	player changes fireDir, until		-- 
+			famSprite:Play("FloatShootUp")                                                                                      --	animCooldown reaches 0				-- 
+			famSprite.FlipX = false                                                                                             ------------------------------------------
+		elseif fireDir == Direction.LEFT and not (famSprite:IsPlaying("FloatShootSide") and famSprite.FlipX)then                
+			famSprite:Play("FloatShootSide")                                                                                    
+			famSprite.FlipX = true --only one animation for the side, have to flip it for left                                  
+		elseif fireDir == Direction.RIGHT and not (famSprite:IsPlaying("FloatShootSide") and not famSprite.FlipX) then          
+			famSprite:Play("FloatShootSide")                                                                                    
+			famSprite.FlipX = false                                                                                             
+		end                                                                                                                     
+		famData.animCooldown = famData.animCooldown - 1
+	end
+	
+	if fireDir == Direction.NO_DIRECTION and not famSprite:IsPlaying("FloatDown") then                                           -----------------------------------------
+		if famData.noDirCooldown == 0 then                                                                                       --  If the player stops firing, but the--
+			famSprite:Play("FloatDown")                                                                                          --  familiar isn't playing the right   --
+			famSprite.FlipX = false                                                                                              --  anim, it'll set it to FloatDown    --
+			famData.noDirCooldown = TinyTinyHorn.maxAnimCooldown                                                                    --  after a cooldown                   --
+		else																													 -----------------------------------------
+			famData.noDirCooldown = famData.noDirCooldown - 1
+		end
+	elseif fireDir ~= Direction.NO_DIRECTION then
+		famData.noDirCooldown = TinyTinyHorn.maxAnimCooldown
+	end
+	
+	for entry, orb in pairs(famData.Orbs) do
 		if not orb:Exists() then
-			data.Orbs[entry] = nil --delete all orbs that were killed so new ones can be made
+			famData.Orbs[entry] = nil --delete all orbs that were killed so new ones can be made
 		else
-			local tmpTarget = Agony:getNearestEnemy(orb)
+			local tmpTarget = Agony:getNearestEnemy(orb) --set nearest target
 			if tmpTarget == orb or tmpTarget == player then
 				orb.Target = fam
 			else
@@ -71,11 +111,13 @@ end
 
 --called on init
 function TinyTinyHorn:initFam(fam)
-	local data = fam:GetData()
-	data.Cooldown = data.Cooldown or 0
-	data.Orbs = data.Orbs or {}
-	--TinyTinyHorn.dbg = data
-	--TinyTinyHorn.dbg2 = data.Orbs
+	local famData = fam:GetData()
+	famData.Cooldown = famData.Cooldown or 0
+	famData.Orbs = famData.Orbs or {}
+	famData.animCooldown = 0
+	famData.noDirCooldown = 0
+	--TinyTinyHorn.dbg = famData
+	--TinyTinyHorn.dbg2 = famData.Orbs
 	fam:GetSprite():Play("FloatDown")
 	fam.IsFollower = true --important, or else it isn't a familiar that follows the player
 end
