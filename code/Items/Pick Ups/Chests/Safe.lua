9ac32f6078cc66e923115ca392db56cb52eb90ac
ProbablyTez <GiulianoOliveri@outlook.com>
2017-03-18 19:44:18 +0100

    Fixed a visual bug with Safe

--- code/Items/Pick Ups/Chests/Safe.lua
+++ code/Items/Pick Ups/Chests/Safe.lua
@@ -16,23 +16,27 @@ function safe:onUpdate()
 
   for _,entity in pairs(ents) do
     if entity.Type == EntityType.ENTITY_PICKUP 
-    and entity.Variant == PickupVariant.AGONY_PICKUP_SAFE 
-    and entity.SubType == ChestSubType.CHEST_CLOSED then 
-      local entdata = entity:GetData()
+    and entity.Variant == PickupVariant.AGONY_PICKUP_SAFE then
       local entsprite = entity:GetSprite()
-      if entdata.storedItem == nil then
-        entdata.storedItem = saveData.safeSpace.storedItem
-      end
-      if player.Position:Distance(entity.Position) <= player.Size + entity.Size + 8
-        and entity.SubType ~= ChestSubType.CHEST_OPENED then
-
-        entsprite:Play("Open", true)
-        sound:Play(SoundEffect.SOUND_PLOP, 1, 0, false, 1)
-        --spawn item
-        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, entdata.storedItem , Isaac.GetFreeNearPosition(player.Position, 50), Vector (0,0), player)
-        saveData.safeSpace.storedItem = 0
-        entity.SubType = ChestSubType.CHEST_OPENED
-        Agony:SaveNow()
+      if entity.SubType == ChestSubType.CHEST_CLOSED then 
+        local entdata = entity:GetData()
+        if entdata.storedItem == nil then
+          entdata.storedItem = saveData.safeSpace.storedItem
+        end
+        if player.Position:Distance(entity.Position) <= player.Size + entity.Size + 8
+          and entity.SubType ~= ChestSubType.CHEST_OPENED then
+          entsprite:Play("Open", true)
+          sound:Play(SoundEffect.SOUND_PLOP, 1, 0, false, 1)
+          --spawn item
+          Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, entdata.storedItem , Isaac.GetFreeNearPosition(player.Position, 50), Vector (0,0), player)
+          saveData.safeSpace.storedItem = 0
+          entity.SubType = ChestSubType.CHEST_OPENED
+          Agony:SaveNow()
+        end
+      elseif entity.SubType == ChestSubType.CHEST_OPENED then
+        if not entsprite:IsPlaying("Open") and not entsprite:IsPlaying("Opened") then
+          entsprite:Play("Opened", true)
+        end
       end
     end
   end
