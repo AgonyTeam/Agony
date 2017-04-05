a9cb1fca8b16d740b2450ac07ab68313038f9298
dedChar <unnamed0736@gmail.com>
2017-04-05 18:17:03 +0200

    The Fireplace: Spawns the correct fireplace entity now

--- code/Items/Collectibles/TheFireplace.lua
+++ code/Items/Collectibles/TheFireplace.lua
@@ -1,13 +1,21 @@
 local theFireplace =  {}
 
 function theFireplace:onUse()
-	local room = Game():GetRoom()
-	local fire = nil
 	local player = Game():GetPlayer(0)
 	local rng = player:GetCollectibleRNG(CollectibleType.AGONY_C_THE_FIREPLACE)
+	local v = nil
+	local chance = rng:RandomFloat()
+	if chance < 0.375 then
+		v = 0 
+	elseif chance < 0.75 then
+		v = 1 --normal and red fire 37.5% chance each
+	elseif chance < 0.875 then
+		v = 2
+	else 
+		v = 3 --blue and purple only 12.5% each
+	end
 	
-	fire = GridEntityType.GRID_FIREPLACE
-	room:SpawnGridEntity(room:GetGridIndex(Isaac.GetFreeNearPosition(Game():GetPlayer(0).Position, 75)), fire, 0, RNG():GetSeed(), 1)
+	Isaac.Spawn(EntityType.ENTITY_FIREPLACE, v, 0, Isaac.GetFreeNearPosition(player.Position, 75), Vector(0,0), player)
 	return true
 end
 
