807a0a38ac0b1e09e2395d1421e1c097e2f994f3
ProbablyTez <GiulianoOliveri@outlook.com>
2017-04-17 18:43:08 +0200

    Yeux rev tweaks yay

--- code/Items/Collectibles/YeuxRevolver.lua
+++ code/Items/Collectibles/YeuxRevolver.lua
@@ -1,6 +1,4 @@
---local item_YeuxRev = Isaac.GetItemIdByName("Yeux Revolver");
 local yeuxRev =  {
-	TearBool = false
 }
 
 function yeuxRev:cacheUpdate (player,cacheFlag)
@@ -12,7 +10,7 @@ function yeuxRev:cacheUpdate (player,cacheFlag)
 			player.ShotSpeed = (player.ShotSpeed + 1.69420*player:GetCollectibleNum(CollectibleType.AGONY_C_YEUX_REVOLVER))
 		end
 		if (cacheFlag == CacheFlag.CACHE_FIREDELAY) then
-			yeuxRev.TearBool = true
+			player.MaxFireDelay = player.MaxFireDelay + 2*player:GetCollectibleNum(CollectibleType.AGONY_C_YEUX_REVOLVER);
 		end
 	end
 end
@@ -24,47 +22,16 @@ function yeuxRev:onUpdate()
     for _,entity in pairs(ents) do
     	if entity.Type == EntityType.ENTITY_TEAR then
     		if entity.FrameCount == 0 then
-    			entity:GetSprite():Load("gfx/effect/tear_yeuxrevolver.anm2", true)
-    			local anim = ""
-    			if player.Damage < 3.5 then
-    				anim = "RegularTear7"
-    			elseif player.Damage < 10 then
-					anim = "RegularTear7"
-    			else
-    				anim = "RegularTear7"
-    			end
+    			--TODO; spawn a white flash on the tear pos
+          		Game():ShakeScreen(math.floor(player.Damage/2))
+    			entity:GetSprite():ReplaceSpritesheet(0, "gfx/effect/tear_yeuxrevolver.png")
           		entity:GetSprite():LoadGraphics()
-          		entity:GetSprite():Play(anim, true)
-          		local v1 = Vector(0, 1)
-          		local v2 = entity.Velocity:Normalized()
-          		local angle = math.acos(v1:Dot(v2))
           		entity:GetSprite().Rotation = entity.Velocity:GetAngleDegrees()
-    		end
+          	end
     	end
     end
   end
 end
 
---FireDelay workaround
-function yeuxRev:updateFireDelay()
-	local player = Isaac.GetPlayer(0)
-	if (yeuxRev.TearBool == true) then
-		player.MaxFireDelay = player.MaxFireDelay + 4*player:GetCollectibleNum(CollectibleType.AGONY_C_YEUX_REVOLVER);
-		yeuxRev.TearBool = false;
-	end
-end
-
-function yeuxRev:onTakeDmg(hurtEntity, dmgAmount, dmgFlags, source, countdown)
-	local player = Game():GetPlayer(0)
-	if player:HasCollectible(CollectibleType.AGONY_C_YEUX_REVOLVER) and hurtEntity:IsVulnerableEnemy() then
-		local time = Isaac.GetTime()
-		while time + 45 >Isaac.GetTime() do
-		end
-		Game():ShakeScreen(math.floor(player.Damage/3))
-	end
-end
-
 Agony:AddCallback(ModCallbacks.MC_POST_UPDATE, yeuxRev.onUpdate)
-Agony:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, yeuxRev.onTakeDmg)
-Agony:AddCallback(ModCallbacks.MC_POST_UPDATE, yeuxRev.updateFireDelay)
 Agony:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, yeuxRev.cacheUpdate)
